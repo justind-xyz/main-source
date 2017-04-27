d0da84b6e61d010ce3ea998cb4125af93c31cee7
justindalzell <justin.d.dalzell@gmail.com>
2017-04-27 04:45:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,24 @@
 
 import webapp2
 
+form="""
+<form method="post" action="/testform">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello JD world!')
-
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+        self.response.write(form)
+        
+class TestHandler(webapp2.RequestHandler):
+	def get(self):
+		q = self.request.get("q")
+		self.response.out.write(q)
+		#self.response.headers['Content-Type'] = 'text/html'
+		self.response.out.write(self.request)
+		
+app = webapp2.WSGIApplication([('/', MainPage),
+								('/testform', TestHandler)],
+								debug=True)
